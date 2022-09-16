98a28aab1217576f9878974a5206f5bfc4579233
Isabella Lopes <icmlopes@gmail.com>
2022-09-16 00:27:24 -0300

    atualização

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Corpo from "./Corpo"
+import SideBar from "./SideBar"
+import NavBar from "./NavBar"
+import User from "./User"
+
+export default function App() {
+    return (
+        <div>
+            <NavBar />
+            <Corpo />
+            <div class="fundo-mobile">
+                <ion-icon name="home"></ion-icon>
+                <ion-icon name="search-outline"></ion-icon>
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
+        </div>
+    );
+}
