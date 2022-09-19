b9b7ef9899b2535035064a3f6a5093322c741874
Isabella Lopes <icmlopes@gmail.com>
2022-09-19 17:33:24 -0300

    Atualização

--- src/Posts.js
+++ src/Posts.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function PerfilPost(props) {
   console.log(props.index)
 
@@ -14,7 +16,7 @@ function PerfilPost(props) {
           <ion-icon name="ellipsis-horizontal"></ion-icon>
         </div>
       </div>
-   
+  
       <div class="conteudo">
       <img src={props.conteudo} alt="Foto feed" />
       </div>
@@ -65,75 +67,3 @@ export default function Posts() {
     </ul>
   )
 }
-
-/* <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/meowed.svg" alt="Foto perfil"/>
-                meowed
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/gato-telefone.svg" alt="Foto perfil"/>
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/respondeai.svg" alt="Foto perfil"/>
-                <div class="texto">
-                  Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/barked.svg" alt="Foto perfil"/>
-                barked
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/dog.svg" alt="Foto perfil"/>
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" alt="Foto perfil"/>
-                <div class="texto">
-                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div> */
--- src/Usuario.js
+++ src/Usuario.js
@@ -2,19 +2,21 @@ import React from "react"
 
 export default function Usuario() {
 
-    
+    const listaPerfil = [
+        { nome: "catanacomics", imagem: "assets/img/catanacomics.svg" }
+    ]
 
     const [nome, setNome] = React.useState("catanacomics")
     const [foto, setFoto] = React.useState("assets/img/catanacomics.svg")
 
-    function alterarFoto(){
-        const fotoPrompt = prompt("Link da foto aqui!")
-        setFoto(fotoPrompt)
-    }
+    // function alterarFoto(){
+    //     const fotoPrompt = prompt("Link da foto aqui!")
+    //     setFoto(fotoPrompt)
+    // }
 
     return (
         <div class="usuario">
-            <img onClick={alterarFoto} src={foto}/>
+            <img onClick={() => setFoto(prompt("Insira o link da foto aqui!"))} src={foto}/>
             <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
